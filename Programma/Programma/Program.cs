3cd043059e1a3d68c06e2130f723080bf0c77646
Fisio <leonardolazzarin14@gmail.com>
2018-11-30 09:35:00 +0100

    Primo avvio

--- Programma/Programma/Program.cs
+++ Programma/Programma/Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Programma
 {
     static class Program
     {
+        public static MySqlConnection connection;
 
         /// <summary>
         /// Punto di ingresso principale dell'applicazione.
@@ -17,7 +19,9 @@ namespace Programma
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            try { connection = new MySqlConnection("Server=127.0.0.1;Database=esami ecdl;Uid=root;Psw=;"); }
+            catch { Application.Run(new Login()); }
+            Application.Run(new Principale());
         }
     }
 }
