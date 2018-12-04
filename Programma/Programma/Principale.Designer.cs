3baa3c9195fba602d6adca502e68daa24effbeb8
Fisio <leonardolazzarin14@gmail.com>
2018-12-04 10:07:19 +0100

    asd

--- Programma/Programma/Principale.Designer.cs
+++ Programma/Programma/Principale.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.leggi = new System.Windows.Forms.Button();
             this.scrivi = new System.Windows.Forms.Button();
             this.panelLeggi = new System.Windows.Forms.Panel();
@@ -49,16 +48,6 @@
             this.panelScrivi.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grigliaScrivi)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(247, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "GESTIONALE";
             // 
             // leggi
             // 
@@ -185,6 +174,7 @@
             // 
             // materialFlatButton1
             // 
+            this.materialFlatButton1.AutoSize = true;
             this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.materialFlatButton1.Depth = 0;
             this.materialFlatButton1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
@@ -196,7 +186,7 @@
             this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialFlatButton1.Name = "materialFlatButton1";
             this.materialFlatButton1.Primary = false;
-            this.materialFlatButton1.Size = new System.Drawing.Size(80, 25);
+            this.materialFlatButton1.Size = new System.Drawing.Size(50, 36);
             this.materialFlatButton1.TabIndex = 7;
             this.materialFlatButton1.Text = "Leggi";
             this.materialFlatButton1.UseVisualStyleBackColor = true;
@@ -204,6 +194,7 @@
             // 
             // materialFlatButton2
             // 
+            this.materialFlatButton2.AutoSize = true;
             this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.materialFlatButton2.Depth = 0;
             this.materialFlatButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -212,7 +203,7 @@
             this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialFlatButton2.Name = "materialFlatButton2";
             this.materialFlatButton2.Primary = false;
-            this.materialFlatButton2.Size = new System.Drawing.Size(80, 25);
+            this.materialFlatButton2.Size = new System.Drawing.Size(54, 36);
             this.materialFlatButton2.TabIndex = 8;
             this.materialFlatButton2.Text = "Scrivi";
             this.materialFlatButton2.UseVisualStyleBackColor = true;
@@ -230,7 +221,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.scrivi);
             this.Controls.Add(this.leggi);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.panelLeggi);
             this.Name = "Principale";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -246,8 +236,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button leggi;
         private System.Windows.Forms.Button scrivi;
         private System.Windows.Forms.Panel panelLeggi;
