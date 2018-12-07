6358e0900c6263d35c9e7b9d1027ded91a67e671
Fisio <leonardolazzarin14@gmail.com>
2018-12-07 09:12:44 +0100

    Aggiunta modifica

--- Programma/Programma/Principale.cs
+++ Programma/Programma/Principale.cs
@@ -60,7 +60,18 @@ namespace Programma
 
         private void modica_Click(object sender, EventArgs e)
         {
-
+            int index = getIndex();
+            if(index > -1)
+            {
+                List<string> colonne = new List<string>();
+                foreach (DataGridViewColumn item in grigliaValori.Columns)
+                    colonne.Add(item.HeaderText);
+                List<string> campi = new List<string>();
+                foreach (DataGridViewCell item in grigliaValori.Rows[index].Cells)
+                    campi.Add(item.Value.ToString());
+                new Modifiche(colonne, campi, comboBox.Text).Show();
+                leggiDatabase();
+            }
         }
 
         private void aggiungi_Click(object sender, EventArgs e)
@@ -68,7 +79,8 @@ namespace Programma
             List<string> colonne = new List<string>();
             foreach (DataGridViewColumn item in grigliaValori.Columns)
                 colonne.Add(item.HeaderText);
-            new Modifiche(colonne,comboBox.Text).ShowDialog();
+            new Modifiche(colonne, comboBox.Text).ShowDialog();
+            leggiDatabase();
         }
 
         void query(MySqlDataReader reader)
@@ -100,34 +112,47 @@ namespace Programma
             {
                 grigliaValori.Rows.Add(item[0]);
                 for (int i = 1; i < item.Length; i++)
-                    grigliaValori.Rows[grigliaValori.Rows.Count - 1].Cells[i].Value = item[i];
+                    grigliaValori.Rows[grigliaValori.Rows.Count - 2].Cells[i].Value = item[i];
             }
         }
 
         void elimina()
         {
-            DialogResult result = MessageBox.Show("Desideri elimare il campo selezionato", "ATTENZIONE", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
-            if(result == DialogResult.Yes)
-                if(grigliaValori.SelectedCells.Count > 0 || grigliaValori.SelectedRows.Count > 0)
-                {
-                    query(new MySqlCommand("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = '" + comboBox.Text + 
-                                           "' AND TABLE_SCHEMA = '" + Program.database + "'", 
-                    Program.connection).ExecuteReader());
-                    string condizione = "";
-                    int index = 0;
-                    if (grigliaValori.SelectedCells.Count == 1)
+            string condizione = "";
+            int index = getIndex();
+            if (index > -1)
+            {
+                DialogResult result = MessageBox.Show("Desideri elimare il campo selezionato", "ATTENZIONE", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                    if (grigliaValori.SelectedCells.Count > 0 || grigliaValori.SelectedRows.Count > 0)
                     {
-                        if (grigliaValori.SelectedCells[0].RowIndex != grigliaValori.RowCount - 1)
-                            index = grigliaValori.SelectedCells[0].RowIndex;
+                        query(new MySqlCommand("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = '" + comboBox.Text +
+                                               "' AND TABLE_SCHEMA = '" + Program.database + "'",
+                        Program.connection).ExecuteReader());
+                        for (int i = 0; i < dati[0].Length; i++)
+                            condizione += dati[0][i] + " = '" + grigliaValori.Rows[0].Cells[i].Value.ToString() + "' AND";
+                        condizione = condizione.Remove(condizione.Length - 4, 4);
+                        try {
+                            new MySqlCommand("DELETE FROM " + comboBox.Text + " WHERE " + condizione).ExecuteNonQuery();
+                            leggiDatabase();
+                        }catch(Exception err) { MessageBox.Show(err.Message, "ATTENZIONE", MessageBoxButtons.OK, MessageBoxIcon.Warning); }
                     }
-                    else if (grigliaValori.SelectedRows.Count == 1)
-                    {
-                        index = grigliaValori.SelectedRows[0].Index;
-                    }
-                    for (int i = 0; i < dati.Count; i++)
-                        condizione += dati[i] + " = '" + grigliaValori.Rows[0].Cells[i].Value.ToString() + "' AND";
-                    condizione = condizione.Remove(condizione.Length - 4, 3);
-                }
+            }
+        }
+
+        int getIndex()
+        {
+            int index = -1;
+            if (grigliaValori.SelectedCells.Count == 1)
+            {
+                if (grigliaValori.SelectedCells[0].RowIndex != grigliaValori.RowCount - 1)
+                    index = grigliaValori.SelectedCells[0].RowIndex;
+            }
+            else if (grigliaValori.SelectedRows.Count == 1)
+            {
+                index = grigliaValori.SelectedRows[0].Index;
+            }
+            return index;
         }
     }
 }
