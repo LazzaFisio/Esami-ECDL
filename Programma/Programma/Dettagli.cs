06f215c687398c8d417f8e9ef2d0f4fd4f2b2d6c
MarcoBaricchio <marco.baricchio@gmail.com>
2019-01-02 11:45:40 +0100

    Crezione form dettagli

---
+++ Programma/Programma/Dettagli.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Animations;
+using MaterialSkin.Controls;
+
+namespace Programma
+{
+    public partial class Dettagli : MaterialForm
+    {
+        public static string durata;
+
+        public Dettagli(string sessione, string esame)
+        {            
+            InitializeComponent();
+            lblSessione.Text = sessione;
+            lblEsame.Text = esame;
+        }
+
+        private void Dettagli_Load(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Sei sicuro di voler inserire questa durata?", "Attenzione", MessageBoxButtons.YesNoCancel);
+            if (result == DialogResult.Yes)
+            {
+                durata = txtDurata.Text;
+                this.Close();
+            }
+        }
+    }
+}
